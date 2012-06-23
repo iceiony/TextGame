f9f0e4c236bc86283ae053bd0048176fea6c9c36
adrian <iceiony@yahoo.com>
2012-06-23 19:30:14 +0100

    Revert "State transfer failing test"
    This reverts commits

--- src/State.js
+++ src/State.js
@@ -1,33 +1,26 @@
 game = {};
 
-game.State = function(transformObject) {
-    this.transform = transformObject;
+game.State = function() {
+       this.stateObj =  JSON.parse(localStorage.persistateState);
 
-   //desereliase state from localStorage
-   this.stateObj =  JSON.parse(localStorage.persistateState);
-
-    //deserialisation step
-    //create circular refferences in given stateObj
-    for(var property in this.stateObj){
-        if(property!=="current"){
-            for(var child in this.stateObj[property]){
-                if(child!=="content"){
-                  var propertyTopKey = this.stateObj[property][child];
-                    this.stateObj[property][child] = this.stateObj[propertyTopKey];
+        //create circular refferences in given stateObj
+        for(var property in this.stateObj){
+            if(property!=="current"){
+                for(var child in this.stateObj[property]){
+                    if(child!=="content"){
+                      var propertyTopKey = this.stateObj[property][child];
+                        this.stateObj[property][child] = this.stateObj[propertyTopKey];
+                    }
                 }
+                this.stateObj[property]["name"] = property;
             }
-            this.stateObj[property]["name"] = property;
         }
-    }
-   this.stateObj["current"] = this.stateObj[this.stateObj["current"]]
-   this.stateObj["current"] = this.stateObj[localStorage.currentStateKey];
-
+       this.stateObj["current"] = this.stateObj[this.stateObj["current"]]
+       this.stateObj["current"] = this.stateObj[localStorage.currentStateKey];
 };
 
 game.State.prototype.transfer = function(symbol){
-    console.log(this.transform);
-    var symbolHash = this.transform.hash(symbol);
-    this.stateObj["current"] = this.stateObj["current"][symbolHash];
+    this.stateObj["current"] = this.stateObj["current"][symbol];
     localStorage.currentStateKey = this.stateObj["current"].name;
     return this.stateObj["current"].content
 };
--- test/StateTransitionTest.js
+++ test/StateTransitionTest.js
@@ -1,13 +1,5 @@
 StateTest = TestCase("When State is used with a ClearTransform");
 
-//create mock tranfser object
-mockTransform = function(){this.wasHashInvoked = false;};
-mockTransform.prototype = new transform.ClearTransform();
-mockTransform.prototype.hash = function(input){
-    this.wasHashInvoked = true;
-    return input;
-}
-
 StateTest.prototype.setUp= function(){
 
    /* given an existing local storage*/
@@ -29,8 +21,7 @@ StateTest.prototype.setUp= function(){
     localStorage.currentStateKey = "current";
 
     /* create a state object as the test subject*/
-    StateTest.mockTransformObj = new mockTransform();
-    StateTest.Subject = new game.State(StateTest.mockTransformObj);
+    StateTest.Subject = new game.State();
 }
 
 StateTest.prototype["test state will transfer for symbol passed"] = function(){
@@ -40,10 +31,5 @@ StateTest.prototype["test state will transfer for symbol passed"] = function(){
 
 StateTest.prototype["test state will transfer for synonim of symbol"] = function(){
    assertSame("Transfer state text",StateTest.Subject.transfer("synonim"));
-   assertSame("testState",localStorage.currentStateKey);
-}
-
-StateTest.prototype["test the State obect uses transformation on imput before transfer "] = function(){
-    StateTest.Subject.transfer("symbol")
-    assertTrue(StateTest.mockTransformObj.wasHashInvoked);
+    assertSame("testState",localStorage.currentStateKey);
 }
