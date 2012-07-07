1731deef58f26af772746f59994887d1f204afb0
adrian <iceiony@yahoo.com>
2012-07-07 16:31:29 +0100

    pass all wild card transition tests

--- src/game/State.js
+++ src/game/State.js
@@ -4,20 +4,37 @@ game.State = function(hashFunction) {
    //desereliase state from localStorage
    this.stateObj =  JSON.parse(localStorage.persistantState);
 
+    //TODO : refactor the next function's replace
+    var createRegex = function(input){
+        input = input.replace("*",".+").replace("*",".+").replace("*",".+").replace("*",".+");
+        return new RegExp(input,"i");
+    };
+
     //deserialisation step
     //create circular refferences in given stateObj
     for(var property in this.stateObj){
         if(property!=="current"){
+            var matches = [];
             for(var child in this.stateObj[property]){
+                if(child.search("\\*")!==-1){
+                    var regObjPair = {
+                                        obj: this.stateObj[this.stateObj[property][child]],
+                                        reg:createRegex(child)
+                                      };
+                    matches.push(regObjPair);
+                    continue;
+                };
                 if(child!=="content"){
                   var propertyTopKey = this.stateObj[property][child];
                     this.stateObj[property][child] = this.stateObj[propertyTopKey];
                 };
+
             };
             this.stateObj[property]["name"] = property;
+            this.stateObj[property]["WildCardMatches"]= matches;
         };
     };
-   this.stateObj["current"] = this.stateObj[this.stateObj["current"]]
+   this.stateObj["current"] = this.stateObj[this.stateObj["current"]];
    this.stateObj["current"] = this.stateObj[localStorage.currentStateKey];
 
 };
@@ -26,9 +43,14 @@ game.State.prototype.transfer = function(symbol){
     if( this.stateObj["current"][symbol]) {
         this.stateObj["current"] = this.stateObj["current"][symbol];
     }
-    else if(this.stateObj["current"]["*"]){
-        console.log("badum");
-        this.stateObj["current"]= this.stateObj["current"]["*"];
+    else {
+        for(index=0;index<this.stateObj["current"]["WildCardMatches"].length;index++) {
+            var regObjPair = this.stateObj["current"]["WildCardMatches"][index];
+            if(symbol.search(regObjPair.reg)==0){
+                this.stateObj["current"]= regObjPair.obj;
+                break;
+            };
+        };
     };
     localStorage.currentStateKey = this.stateObj["current"].name;
     return this.stateObj["current"].content
