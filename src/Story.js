2dfb56e40dc39575962433ec5ae46b2b4d7bb922
adrian <iceiony@yahoo.com>
2012-08-07 15:29:15 +0100

    Create a demo page to run scripts generated
    Also have a 2 state mock story to play with.
    And a SimpleHttpServer to host the files

--- src/Story.js
+++ src/Story.js
@@ -3,5 +3,13 @@
  * Contains the game story as a JSON object
  */
 var Story={
-
+    "initial":{
+        content:"Where am I, but more importantly , WHO am I ?",
+        transitions:{
+            "I am Adrian": "remember"
+        }
+    },
+    "remember":{
+        content:"Yes, that's it, that is my name. How could I forget"
+    }
 };
