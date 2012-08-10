5c9bc235fabf9378f4774784d211ae60ea12b3d9
adrian <iceiony@yahoo.com>
2012-08-10 19:54:34 +0100

    Add first 2 paragraphs of the game.

--- src/Story.js
+++ src/Story.js
@@ -4,12 +4,38 @@
  */
 var Story={
     "initial":{
-        content:"Where am I, but more importantly , WHO am I ?",
+        content:"'What... where am I.. who am I ? I must focus, focus on remembering , my name... what is it ?'</br>" +
+                "Yes! I remember now I'm...",
         transitions:{
-            "I am Adrian": "remember"
+            "I am Adrian": "blame",
+            "My name is *": "blame",
+            "Adrian":"blame"
         }
     },
-    "remember":{
-        content:"Yes, that's it, that is my name. How could I forget"
+    "blame":{
+        content:"And so I've reached the point of my demise. While the cold and darkness are slowly draining my life, shivers and despair drift through my chest. I cannot help but wonder what I have done so wrong. " +
+                "What is it that I cherish most, and the reason for this ill fate? Was it my thirst for knowledge, love, or was it God; has has forsaken me? What or who would I blame ?",
+        transitions:{
+            "*knowledge*": "blame_knowledge",
+            "*love*":"blame_love",
+            "*God*":"blame_god",
+            "*god*":"blame_god",
+            "nothing":"blame_none",
+            "no one": "blame_none",
+            "*": "blame_none"
+        }
+    },
+    "blame_knowledge":{
+        content:"To learn, to know, to bring use to this short existence. What meaning have I found in life other than helping humanity and its future through discovery and invention. Knowledge, as I see, is the only way to achieve such greatness. "
+    },
+    "blame_love":{
+        content:"Love Of course, it has to be it. What else would shape ones will more than fallowing hearts' desires. It must have been you, my love...the one desire above all other."
+    },
+    "blame_god":{
+        content:"Born from His mercy, we live by His good will. What creature would defy its destiny, our Lords' will? Father, I pray that I did not anger you and played my purpose well."
+    },
+    "blame_none":{
+        content:"I must think too highly of myself to believe that there is any importance to the world in my life and death. We are all just insignificant specs in this immense universe. " +
+                "Insignificant and with a pointless existence, we must at least seek happiness while we live."
     }
 };
