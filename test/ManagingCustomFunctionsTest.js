512dc83eb066549b54368f43d3dc96340db687d2
adrian <iceiony@yahoo.com>
2012-08-15 22:00:19 +0100

    New failing tests when loading customs in separate loads

---
+++ test/ManagingCustomFunctionsTest.js
@@ -0,0 +1,67 @@
+"use strict";
+var ManagingCustoms = new TestCase("When loading custom functions in multiple steps and with non duplicate names");
+(function(){
+    var Subject = new Game.Engine(),
+        wasFirstFunctionCalled = false,
+        wasSecondFunctionCalled = false,
+        wasOverwritingFunctionCalled = false;
+
+    ManagingCustoms.prototype.setUp = function(){
+
+        Subject = new Game.Engine();
+        Subject.loadCustom({
+            "firstFunction": function(input){
+                wasFirstFunctionCalled=true;
+                return input;
+            }
+        });
+
+        Subject.loadCustom({
+            "secondFunction": function(input){
+                wasSecondFunctionCalled =true;
+                return input;
+            }
+        });
+
+        var mockStory = {
+            "initial": {
+                content: "Where am I, but more importantly , WHO am I ?",
+                transitions: {
+                    "remember": "I am Adrian"
+                }
+            },
+            "remember": {
+                preRender: ["firstFunction","secondFunction"],
+                content: "Yes, that's it, that is my name. How could I forget"
+            }
+        };
+        Subject.loadStory(mockStory);
+    };
+
+    ManagingCustoms.prototype["test that the first function is executed"] = function(){
+        Subject.process("I am Adrian");
+        assertTrue(wasFirstFunctionCalled);
+    };
+
+    ManagingCustoms.prototype["test that the second function is executed"] = function(){
+        Subject.process("I am Adrian");
+        assertTrue(wasSecondFunctionCalled);
+    };
+
+    ManagingCustoms.prototype["test that loading the same function name will overwrite the previous one"] = function(){
+        wasFirstFunctionCalled = false;
+        wasSecondFunctionCalled = false;
+        wasOverwritingFunctionCalled = false;
+
+        Subject.loadCustom({
+            "secondFunction": function(input){
+                wasOverwritingFunctionCalled = true;
+                return input;
+            }
+        });
+        Subject.process("I am Adrian");
+        assertTrue(wasOverwritingFunctionCalled);
+        assertFalse(wasSecondFunctionCalled);
+    };
+
+}());
