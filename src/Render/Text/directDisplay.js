2f472ebf0d520224ca4265c5aa2426f55255a46e
adrian <iceiony@yahoo.com>
2012-08-20 22:58:00 +0100

    pass directDisplay failing tests for direct html output with no effects

---
+++ src/Render/Text/directDisplay.js
@@ -0,0 +1,11 @@
+"user strict";
+Game.namespace("Game.Render.Text");
+/*
+  This will point to Game.Engine instance;
+  Element returned will be a JQuery object;
+ */
+Game.Render.Text.directDisplay = function(scene){
+    var jQuery = $;
+
+    jQuery(this.getOutputElement()).html(scene.content);
+};
