b3706e263c201c1875de2f27395508c7c3be2b93
adrian <iceiony@yahoo.com>
2012-08-22 00:43:40 +0100

    FIx Test randomly failing because of localPersistance

--- test/AttachingEngineToDOM.js
+++ test/AttachingEngineToDOM.js
@@ -2,6 +2,11 @@
 var AttachingEngineToDOM = new TestCase("When creating a new engine with the DOMParent parameter passed");
 
 (function(){
+    AttachingEngineToDOM.prototype.setUp = function(){
+        delete(localStorage.persistantState);
+        delete(localStorage.currentStateKey);
+    };
+
     AttachingEngineToDOM.prototype["test that the engine attaches to a given DOM element and creates input and output elements"]=function(){
         var element = document.createElement("div"),
             Subject = new Game.Engine({
@@ -10,6 +15,7 @@ var AttachingEngineToDOM = new TestCase("When creating a new engine with the DOM
         assertTrue($(element).find("#input").length>0);
         assertTrue($(element).find("#output").length>0);
     };
+
     AttachingEngineToDOM.prototype["test that the engine attached to given DOM ID and creates input and output elements"]=function(){
         /*:DOC += <div id="container">some text</div> */
         var Subject = new Game.Engine({
@@ -19,6 +25,7 @@ var AttachingEngineToDOM = new TestCase("When creating a new engine with the DOM
         assertTrue($("#input").length>0);
         assertTrue($("#output").length>0);
     };
+
     AttachingEngineToDOM.prototype["test that the engine exposes the parentElement,inputElement and outputElement"] = function(){
         /*:DOC += <div id="container">some text</div> */
         var Subject = new Game.Engine({
@@ -32,6 +39,7 @@ var AttachingEngineToDOM = new TestCase("When creating a new engine with the DOM
         assertSame($("#output").text(),$(Subject.getOutputElement()).text());
         assertSame($("#container").text(),$(Subject.getParentElement()).text());
     };
+
     AttachingEngineToDOM.prototype["test that the engine creates the input inside a form that does not propagate events"] = function(){
         /*:DOC += <div id="container">some text</div> */
         var Subject = new Game.Engine({
@@ -46,11 +54,13 @@ var AttachingEngineToDOM = new TestCase("When creating a new engine with the DOM
                 }
             };
 
+        Subject.process = function(input){};
         inputForm.data("events")["submit"][0].handler(fakeEvent);
 
         assertSame("FORM",inputForm.get(0).tagName);
         assertTrue(wasSubmitNotPropagated);
     };
+
     AttachingEngineToDOM.prototype["test form submit handler does calls the local engine instance process method"] = function(){
         /*:DOC += <div id="container">some text</div> */
         var Subject = new Game.Engine({
