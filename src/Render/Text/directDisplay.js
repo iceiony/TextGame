e1d21924f3aff1074ae0f8155ecade1f6f1f3a3b
adrian <iceiony@yahoo.com>
2012-10-27 21:02:16 +0100

    move typewriter display into it's own namespace and file

--- src/Render/Text/directDisplay.js
+++ src/Render/Text/directDisplay.js
@@ -1,7 +1,7 @@
 "use strict";
 Game.namespace("Game.Render.Text");
 /*
-  This will point to Game.Engine instance;
+  "this" will point to Game.Engine instance;
   Element returned will be a JQuery object;
  */
 Game.Render.Text.directDisplay = function (scene) {
---
+++ src/Render/Text/typeWriterDisplay.js
@@ -0,0 +1,53 @@
+"use strict";
+Game.namespace("Game.Render.Text");
+/*
+ "this" will point to Game.Engine instance;
+ Element returned will be a JQuery object;
+ */
+Game.Render.Text.typeWriterDisplay = function (scene) {
+    var jQuery = $,
+        outputElement = this.getOutputElement(),
+        inputElement = this.getInputElement(),
+        sceneText = scene.content,
+        partialContent = "",
+        segment,
+        delay = 34,
+        contentSize = sceneText.length,
+        index=0,
+        t,
+        enterHandler,
+        isBound = true;
+
+    t = setInterval(function(){
+        if( index === contentSize) {
+            clearInterval(t);
+            isBound = false;
+            return;
+        }
+
+        segment=""+sceneText[index];
+        if(segment === "<"){
+            segment = sceneText.substr(index,sceneText.indexOf('>',index)-index+1);
+            index += segment.length;
+        }
+        else{
+            index += 1;
+        }
+        partialContent += segment;
+        outputElement.html(partialContent);
+    },delay);
+
+    inputElement.bind("keydown",enterHandler = function(event){
+        if(event.keyCode === 13){
+            if(isBound){
+                clearInterval(t);
+                isBound=false;
+                event.preventDefault();
+                outputElement.html(sceneText);
+                return false;
+            }
+            inputElement.unbind("keydown",enterHandler);
+        }
+        return true;
+    });
+};
