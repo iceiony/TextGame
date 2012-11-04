770c5ac6fbeb3a61861e1e00dca043f523f9ecb3
adrian <iceiony@yahoo.com>
2012-11-04 20:25:49 +0000

    inherit glow color from parent

--- src/Render/Text/simpleFadeIn.js
+++ src/Render/Text/simpleFadeIn.js
@@ -7,20 +7,19 @@ Game.Render.Text.simpleFadeIn = function(scene){
         engine = this,
         sceneText = scene.content,
         sceneName = scene.name,
-        enterHandler,
-        delay = 1597;
+        glowHandler,
+        delay = 1597,
+        glowColor = outputElement.css("color");
 
     outputElement.hide();
-    outputElement.addClass("whiteGlow");
+    outputElement.css({textShadow : "0 0 10px "+glowColor});
     outputElement.html(sceneText);
     outputElement.fadeIn(delay);
 
-    inputElement.bind("keydown",enterHandler = function(event){
-        if(event.keyCode === 13 && engine.state.getCurrent().name !== sceneName){
-            outputElement.html(sceneText);
-            outputElement.removeClass("whiteGlow");
-            inputElement.unbind("keydown",enterHandler);
+    inputElement.bind("keydown",glowHandler = function(event){
+        if(event.keyCode === 13){
+            outputElement.css({textShadow : ""});
+            inputElement.unbind("keydown",glowHandler);
         }
-        return true;
     });
 };
