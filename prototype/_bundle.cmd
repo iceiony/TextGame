f122117eb33c932af675137289baad13d192f656
iceiony <iceiony@yahoo.com>
2014-05-18 16:45:00 +0100

    add second web view for A - B Testing

--- prototype/_bundle.cmd
+++ prototype/_bundle.cmd
@@ -6,3 +6,5 @@ echo "************************************************"
 browserify --extension='.coffee' -t coffeeify runner.coffee -o bundle.js -r ./runner
 uglifyjs bundle.js -o bundle.min.js -m -c
 rm bundle.js
+cp bundle.min.js ./WebA/bundle.min.js
+cp bundle.min.js ./WebB/bundle.min.js
