9e78087fbdd9b079f0ea65c3f3a4ce5793a98c02
iceiony <iceiony@yahoo.com>
2015-01-12 21:57:25 +0000

    update bundle script to work with latest version of browserify \n\n browserify 8.1.0

--- prototype/_bundle.bat
+++ prototype/_bundle.bat
@@ -3,8 +3,9 @@ echo "Patch Natural to not require 'WNdb' "
 echo "Patch Sylvester to not require 'lapack'"
 echo "************************************************"
 
-browserify --extension='.coffee' -t coffeeify runner.coffee -o bundle.js -r ./runner -r ./server_logging/logging_client
+browserify --extension='.coffee' -r ./runner.coffee:./runner -r ./server_logging/logging_client.coffee:./server_logging/logging_client -o bundle.js
 uglifyjs bundle.js -o bundle.min.js -m -c
-rm bundle.js
 cp bundle.min.js ./WebA/bundle.min.js
 cp bundle.min.js ./WebB/bundle.min.js
+rm bundle.js
+rm bundle.min.js
